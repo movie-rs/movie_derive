f3a9075b6793de9da3e1c498cee4e9425f323066
Paweł Zmarzły <pawo2500@gmail.com>
2019-03-10 00:34:31 +0100

    initial

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,16 @@ use proc_macro::TokenStream;
 use std::collections::HashMap;
 
 #[proc_macro]
-// input: "SimplestActor gets : Ping , sends : Pong , on_message : Ping => Pong ,"
 pub fn actor(input: TokenStream) -> TokenStream {
+    actor_internal(input, false)
+}
+#[proc_macro]
+pub fn actor_dbg(input: TokenStream) -> TokenStream {
+    actor_internal(input, true)
+}
+
+// input: "SimplestActor gets : Ping , sends : Pong , on_message : Ping => Pong ,"
+fn actor_internal(input: TokenStream, debug: bool) -> TokenStream {
     let input = input.to_string();
 
     // locate attributes inside input string
@@ -16,10 +24,20 @@ pub fn actor(input: TokenStream) -> TokenStream {
         let pos = input.find(&search_str);
         if let Some(pos) = pos {
             locations.push((pos, attr, pos + search_str.len()));
+        } else {
+            // TODO: figure out why in StreamParsingActor example
+            // spaces are replaced by newlines
+            let search_str = format!(" {} :\n", attr);
+            let pos = input.find(&search_str);
+            if let Some(pos) = pos {
+                locations.push((pos, attr, pos + search_str.len()));
+            }
         }
     };
     try_find("gets");
     try_find("sends");
+    try_find("data");
+    try_find("on_init");
     try_find("on_message");
     locations.sort_unstable();
 
@@ -41,6 +59,10 @@ pub fn actor(input: TokenStream) -> TokenStream {
         attrs.insert(locations[i].1, value.to_string());
     }
 
+    if debug {
+        dbg!(&attrs);
+    }
+
     // check for missing required attrs
     if !attrs.contains_key("gets") {
         panic!("Actor must accept some input (consider accepting Tick or Stop) - define enum types in `gets`");
@@ -52,60 +74,70 @@ pub fn actor(input: TokenStream) -> TokenStream {
         panic!("Actor must have on_message handler");
     }
 
+    let has_data = attrs.contains_key("data");
+    let tick =
+        attrs["gets"].find(", Tick , ").is_some() || attrs["gets"].find(", Tick ,\n").is_some();
+
     // assign default values for missing optional supported attrs
     attrs.entry("data").or_insert("".to_string());
     attrs.entry("on_init").or_insert("".to_string());
 
-    let tick = false;
-
+    // TODO: consider rewriting to quote!()
     format!(
         "
         mod {name} {{
         #[derive(Debug)]
-        struct {name} {{
+        pub struct Actor {{
             running: bool,
+            data: Data,
+        }}
+        #[derive(Debug)]
+        pub struct Data {{
             {data}
         }}
-        #[derive(Debug)]
-        enum {name}Input {{
+        #[derive(Debug, PartialEq)]
+        pub enum Input {{
             {gets}
         }}
-        #[derive(Debug)]
-        enum {name}Output {{
+        #[derive(Debug, PartialEq)]
+        pub enum Output {{
             {sends}
         }}
-        impl {name} {{
-            fn start() -> movie::Handle<
+        impl Actor {{
+            pub fn start({optional_data_argument}) -> movie::Handle<
                 std::thread::JoinHandle<()>,
-                {name}Input,
-                {name}Output
-            >
+                Input,
+                Output
+                >
             {{
                 let (tx_ota, rx_ota) = std::sync::mpsc::channel(); // owner-to-actor
                 let (tx_ato, rx_ato) = std::sync::mpsc::channel(); // actor-to-owner
                 let handle = std::thread::spawn(move || {{
-                    let mut actor = {name} {{
-                        running: true,
-                        {data}
+                    {{
+                        // newline in case on_init ends with a comment
+                        {on_init}
                     }};
-                    {{ {on_init} }};
+                    {optional_default_data}
+                    let mut actor = Actor {{
+                        running: true,
+                        data,
+                    }};
                     while actor.running {{
-                        let mut on_message = |message: {name}Input| {{
-                            use {name}Input::*;
-                            use {name}Output::*;
+                        let mut on_message = |message: Input| {{
+                            use Input::*;
+                            use Output::*;
                             match message {{
                                 {on_message}
                             }}
                         }};
                         while let Ok(message) = rx_ota.try_recv() {{
-                            let reply: {name}Output = on_message(message);
+                            let reply: Output = on_message(message);
                             tx_ato.send(reply).unwrap();
                         }}
-                        let reply: Option<{name}Output> = {optional_tick_handler};
+                        let reply: Option<Output> = {optional_tick_handler};
                         if let Some(reply) = reply {{
                             tx_ato.send(reply).unwrap();
                         }}
-
                         // sleep for 4 ms before polling or ticking
                         // 4 ms is minimum on some Linux systems
                         // so it was chosen for compatibility
@@ -123,16 +155,18 @@ pub fn actor(input: TokenStream) -> TokenStream {
         }}
         }}",
         name = attrs["name"],
-        data = attrs["data"],
         gets = attrs["gets"],
         sends = attrs["sends"],
+        data = attrs["data"],
         on_init = attrs["on_init"],
         on_message = attrs["on_message"],
         optional_tick_handler = if tick {
-            "Some(on_message(Tick))"
+            "Some(on_message(Input::Tick))"
         } else {
             "None"
         },
+        optional_data_argument = if has_data { "data: Data" } else { "" },
+        optional_default_data = if has_data { "" } else { "let data = Data {};" },
     )
     .parse()
     .unwrap()
